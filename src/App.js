6ab445622061d2a360b35b9f869a690b01c5d484
sougata <sougata19582001@gmail.com>
2024-02-17 18:17:03 +0530

    added key in prop

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function MovieList() {
   return (
     <ul className="list">
       {movies?.map((movie) => (
-        <Movie movie={movie} />
+        <Movie movie={movie} key={movie.imdbID}/>
       ))}
     </ul>
   );
@@ -143,7 +143,7 @@ function MovieList() {
 
 function Movie({ movie }) {
   return (
-    <li key={movie.imdbID}>
+    <li>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
