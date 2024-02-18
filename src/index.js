4333e77a22a205174b2e862aea4c62cec9f31b37
sougata <sougata19582001@gmail.com>
2024-02-18 18:31:40 +0530

    starting rating

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        defaultRating={3}
+        onSetRating={setMovieRating}
+      />
+      {movieRating > 0 && <p>This movie was rated {movieRating} stars</p>}
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
 
