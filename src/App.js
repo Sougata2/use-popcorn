95a56efb57d1c2256285d352f95d10f1a8e29a64
sougata <sougata19582001@gmail.com>
2024-03-09 18:04:18 +0530

    custom hook for movies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
-
+import { useMovies } from "./useMovies";
 const KEY = "acc82eb2";
+
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 function App() {
-  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [query, setQuery] = useState("naruto");
   const [selectedId, setSelectedId] = useState(null);
-
-  const page = 1;
+  const { movies, isLoading, error } = useMovies(query);
 
   function handleSelectedMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -31,48 +28,48 @@ function App() {
     setWatched((movies) => movies.filter((movie) => movie.imdbID !== id));
   }
 
-  useEffect(() => {
-    const controller = new AbortController();
-    async function fetchMovies() {
-      try {
-        setIsLoading(true);
-        setError(""); // need to reset the error to show the fetched results
-        const res = await fetch(
-          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}&page=${page}`,
-          { signal: controller.signal }
-        );
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   async function fetchMovies() {
+  //     try {
+  //       setIsLoading(true);
+  //       setError(""); // need to reset the error to show the fetched results
+  //       const res = await fetch(
+  //         `https://www.omdbapi.com/?apikey=${KEY}&s=${query}&page=${page}`,
+  //         { signal: controller.signal }
+  //       );
 
-        if (!res.ok)
-          throw new Error("Something went wrong with fetching movies");
+  //       if (!res.ok)
+  //         throw new Error("Something went wrong with fetching movies");
 
-        const data = await res.json();
-        // console.log(data);
+  //       const data = await res.json();
+  //       // console.log(data);
 
-        if (data.Response === "False") throw new Error("Movie not found.");
+  //       if (data.Response === "False") throw new Error("Movie not found.");
 
-        setMovies((movies) => data.Search);
-        setError("");
-      } catch (err) {
-        if (err.name !== "AbortError") {
-          setError(err.message);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    }
+  //       setMovies((movies) => data.Search);
+  //       setError("");
+  //     } catch (err) {
+  //       if (err.name !== "AbortError") {
+  //         setError(err.message);
+  //       }
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
 
-    if (!query.length) {
-      setMovies([]);
-      setError("");
-      return;
-    }
-    handleCloseMovie();
-    fetchMovies();
+  //   if (!query.length) {
+  //     setMovies([]);
+  //     setError("");
+  //     return;
+  //   }
+  //   handleCloseMovie();
+  //   fetchMovies();
 
-    return function () {
-      controller.abort();
-    };
-  }, [query]);
+  //   return function () {
+  //     controller.abort();
+  //   };
+  // }, [query]);
   return (
     <>
       <NavBar movies={movies}>
